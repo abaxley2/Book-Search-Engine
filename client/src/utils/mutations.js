4380d8d97846616864e9f17e6ed332d54393feef
Austin Baxley <ausbaxley@gmail.com>
2021-08-24 17:19:36 -0400

    added useMutation hook to SearchBooks.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,67 @@
 import { gql } from "@apollo/client";
 
 // export login user mutation
-export const LOGIN_USER = gql``;
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String, $password: String) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
 // export add user mutation
-export const ADD_USER = gql``;
+export const ADD_USER = gql`
+  mutation addUser($username: String, $email: String, $password: String) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
 // export save book mutation
-export const SAVE_BOOK = gql``;
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;
 
 // export remove book mutation
-export const REMOVE_BOOK = gql``;
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookData: BookInput) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;

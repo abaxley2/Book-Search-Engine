11226a80a8db1755c9a31981226e8ba38b00cd38
Austin Baxley <ausbaxley@gmail.com>
2021-08-24 17:47:37 -0400

    removed useEffect hook and pulled in GET_ME query and REMOVE_BOOK mutation

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -28,7 +28,7 @@ const SearchBooks = () => {
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
   //added useMutation hook
-  const [saveBook, { error }] = useMutation(SAVE_BOOK);
+  const [saveBook] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup

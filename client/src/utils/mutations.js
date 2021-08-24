7dbf6c860b7c7e733622dcaafad2ec02cb8edd46
Austin Baxley <ausbaxley@gmail.com>
2021-08-24 18:29:49 -0400

    fixed typeDefs and mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,8 +1,7 @@
 import { gql } from "@apollo/client";
 
-// export login user mutation
 export const LOGIN_USER = gql`
-  mutation loginUser($email: String, $password: String) {
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
@@ -13,9 +12,8 @@ export const LOGIN_USER = gql`
   }
 `;
 
-// export add user mutation
 export const ADD_USER = gql`
-  mutation addUser($username: String, $email: String, $password: String) {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
@@ -26,40 +24,36 @@ export const ADD_USER = gql`
   }
 `;
 
-// export save book mutation
 export const SAVE_BOOK = gql`
-  mutation saveBook($bookData: BookInput) {
+  mutation saveBook($bookData: BookInput!) {
     saveBook(bookData: $bookData) {
       _id
       username
       email
-      bookCount
       savedBooks {
         bookId
         authors
-        title
-        description
         image
+        description
+        title
         link
       }
     }
   }
 `;
 
-// export remove book mutation
 export const REMOVE_BOOK = gql`
-  mutation removeBook($bookData: BookInput) {
-    saveBook(bookData: $bookData) {
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
       _id
       username
       email
-      bookCount
       savedBooks {
         bookId
         authors
-        title
-        description
         image
+        description
+        title
         link
       }
     }
